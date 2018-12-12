a96ebd9cfb8aad1702079c3ce509704bc4877e51
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-12 10:26:02 +0530

    Remove all routes and associated functions and place in a seperate module

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
 const express = require('express')
 
-const mailgunConfig = require('./config.js')()
-const mailgun = require('mailgun-js')(mailgunConfig)
-
 const app = express()
-
-var data = {
-  from : 'Anonymous <me@samples.mailgun.org>',
-  to : 'vinayakrugvedi@gmail.com', //Only one authorized recipient
-  subject : 'Verifying You!',
-  html: `<p>Click the below link to verify : </p>
-         <a href="https://www.google.com">Click Here!</a>`
-}
+const routes = require('./routes')
 
 const port = process.env.port || 5000
 
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:false}))
-
-app.post('/userVerify', (req, res) => {
-  mailgun.messages().send(data, (err, resBody) => {
-    if(err) {
-      console.log(err)
-      res.status(err.statusCode).end()
-    } else {
-      console.log(resBody)
-      res.status(201).end()
-    }
-  })
-})
+app.use(express.json())
+app.use('/', routes)
 
 app.listen(port, () => {
   console.log(`SERVER : Listening on port ${port}`)
