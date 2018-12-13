10fff0dac720ffb0f70d53039312b066ea14ecac
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-14 03:04:40 +0530

    Add expiration logic, modify naming conventions

--- handleDB.js
+++ handleDB.js
@@ -7,22 +7,22 @@ const client = new Client({connectionString})
 client.connect()
 
 function store(data, res) {
-  const query = {
+  const insertQuery = {
     name : 'store-user-info',
     text : 'INSERT INTO data(token, email, expires, password, verified) VALUES($1, $2, $3, $4, $5) RETURNING *',
     values : [`{${data.token}}`, `{${data.email}}`, data.expires, `{${data.password}}`, data.verified]
   }
 
-  client.query(query, (err, result) => {
-    if(err) {
-      console.log(err)
+  client.query(insertQuery, (insertErr, insertResult) => {
+    if(insertErr) {
+      console.log(insertErr)
 
       res.status(502).json({
         msg : 'Oops!, couldnt save your data in the DB; Try again...'
       })
 
     } else {
-      console.log(result.rows)
+      console.log(insertResult.rows)
 
       // Upon storing, send a Verification link
       sendVerificationLink(data.email, data.token, res)
@@ -32,44 +32,71 @@ function store(data, res) {
 
 function fetch(token, res) {
   console.log(token, 'in fetch')
-  const query = {
+  const fetchQuery = {
     name : 'get-users-data',
     text : `SELECT * FROM data where token[1] = '${token}'`
   }
 
-  client.query(query, (err, result) => {
-    if(err) {
-      console.log(err)
+  client.query(fetchQuery, (fetchErr, fetchResult) => {
+    if(fetchErr) {
+      console.log(fetchErr)
 
       res.status(502).json({
         msg : 'Couldnt retrive data; Try clicking the link again after some time...'
       })
 
     } else {
-      console.log(result.rows[0].expires)
+      console.log(fetchResult.rows[0].expires)
 
       const updateQuery = {
         name : 'set-verified-to-true',
         text : 'UPDATE data SET verified = true WHERE id = $1 RETURNING *',
-        values : [result.rows[0].id]
+        values : [fetchResult.rows[0].id]
       }
 
-      client.query(updateQuery, (err, result) => {
-        if(err) {
-          console.log(err)
-          res.status(502).json({
-            msg : 'Couldnt set data; Try clicking the link again after some time...'
-          })
-        } else {
-          console.log(result)
-          res.status(201).json({
-            status : 'Congratulations, You are verified...'
-          })
+      let isLinkValid = !isExpired(fetchResult.rows[0].expires)
+      if(isLinkValid) {
+        client.query(updateQuery, (updateErr, updateResult) => {
+          if(updateErr) {
+            console.log(updateErr)
+            res.status(502).json({
+              msg : 'Couldnt set data; Try clicking the link again after some time...'
+            })
+          } else {
+            console.log(updateResult)
+            res.status(201).json({
+              msg : 'Congratulations, You are verified...'
+            })
+          }
+        })
+      } else {
+        const deleteQuery = {
+          name : 'delete-user-data',
+          text : 'DELETE FROM data WHERE id = $1',
+          values : [fetchResult.rows[0].id]
         }
-      })
 
+        client.query(deleteQuery, (deleteErr, deleteResult) => {
+          if(deleteErr) {
+            console.log(deleteErr)
+            res.status(502).json({
+            msg : 'Oops!, something went wrong; Try again...'
+            })
+          } else {
+            console.log(deleteResult)
+            res.status(201).json({
+            msg : 'The verification link has expired; Kindle re-register at the registration page'
+            })
+          }
+        })
+      }
     }
   })
+}
+
+function isExpired(time) {
+  let currentTime = Date.now()
+  return currentTime > time ? true : false
 }
 
 const handleDB = {
