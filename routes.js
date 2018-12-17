90ed800a9d1b04995871e8ac20efaea151260bc4
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-18 02:22:29 +0530

    Add route for authenticating user

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const performValidations = require('./controllers/validateEmailAndPassword')
 const hashPasswordAndStoreInDB = require('./controllers/hashPasswordAndStoreInDB')
+const authenticateTheUser = require('./controllers/authenticateTheUser')
 const handleDB = require('./controllers/handleDB')
 
 router.use('/verifyUser', performValidations)
@@ -14,5 +15,8 @@ router.post('/verifyUser', hashPasswordAndStoreInDB)
 
 //When the user clicks the verification link, update his/her status in DB
 router.get('/verifyMe/:token', handleDB.updateUsersVerifiedStatus)
+
+router.use('/authenticateUser', performValidations)
+router.post('/authenticateUser', authenticateTheUser)
 
 module.exports = router
