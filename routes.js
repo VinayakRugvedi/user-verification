7356e816086c3b35f0f75366028bb3d5a36a9fef
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-18 00:56:10 +0530

    Use controllers as handlers fun

--- routes.js
+++ routes.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const router = express.Router()
+
 const performValidations = require('./controllers/validateEmailAndPassword')
 const hashPasswordAndStoreInDB = require('./controllers/hashPasswordAndStoreInDB')
-
-const handleDB = require('./handleDB')
+const handleDB = require('./controllers/handleDB')
 
 router.use('/verifyUser', performValidations)
 
-router.use('/verifyUser', (req, res, next) => {
-  //Cross check if user has already registered
-  handleDB.preCheck(req.body.email, res, next) //cb
-})
+//Cross check if user has already registered
+router.use('/verifyUser', handleDB.checkIfUserRegistered)
 
 router.post('/verifyUser', hashPasswordAndStoreInDB)
 
-router.get('/verifyMe/:token', (req, res) => {
-  //When the user clicks the verification link, update his/her status in DB
-  console.log(req.params.token)
-  handleDB.fetch(req.params.token, res)
-})
+//When the user clicks the verification link, update his/her status in DB
+router.get('/verifyMe/:token', handleDB.updateUsersVerifiedStatus)
 
 module.exports = router
