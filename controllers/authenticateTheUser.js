d7aa3d8cc635e5a15fc9de3e2c3e05b542321345
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-19 23:52:59 +0530

    make func async and execute compare in the same scope

--- controllers/authenticateTheUser.js
+++ controllers/authenticateTheUser.js
@@ -2,29 +2,30 @@ const bcrypt = require('bcrypt')
 
 const handleDB = require('./handleDB')
 
-function authenticateTheUser(req, res, next) {
-  console.log(req.body)
-  function compare(hash) {
-    bcrypt.compare(req.body.password, hash, (err, result) => {
-      if(err) {
-        console.log(err)
-        res.status(500).json({
-          msg : "Oops!, something went wrong; Try again..."
+async function authenticateTheUser(req, res, next) {
+  let hash = await handleDB.getHashIfAvailable(req.body.email, res)
+  if(hash) compare(hash, req, res)
+}
+
+function compare(hash, req, res) {
+  bcrypt.compare(req.body.password, hash, (err, result) => {
+    if(err) {
+      console.log(err)
+      res.status(500).json({
+        msg : "Oops!, something went wrong; Try again..."
+      })
+    } else {
+      console.log(result)
+      if(result) //result is boolean
+        res.status(201).json({
+          msg : "You have been successfully authenticated"
         })
-      } else {
-        console.log(result)
-        if(result)
-          res.status(201).json({
-            msg : "You have been successfully authenticated"
-          })
-        else
-          res.status(401).json({
-            msg : "Your credentials seems to be incorrect!"
-          })
+      else
+        res.status(401).json({
+          msg : "Your credentials seems to be incorrect!"
+        })
       }
-    })
-  }
-  handleDB.getHashIfAvailable(req.body.email, compare, res)
+  })
 }
 
 module.exports = authenticateTheUser
