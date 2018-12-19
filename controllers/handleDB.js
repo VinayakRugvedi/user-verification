a7ae01b98ce396b6bf60f71794b7fadbab2d57a6
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-19 23:57:42 +0530

    Make functions async, seperate all db interaction into model

--- controllers/handleDB.js
+++ controllers/handleDB.js
@@ -1,124 +1,103 @@
-const client = require('../model/postgresql/setup')
-const queries = require('../model/postgresql/queries')
-
+const postgresql = require('../model/postgresql')
 const sendVerificationLink = require('./sendVerificationLink')
 
-function storeUserDataInDB(data, res) {
-  const insertQuery = queries.insertUserData(data)
-  client.query(insertQuery, (insertErr, insertResult) => {
-    if(insertErr) {
-      console.log(insertErr)
-      res.status(502).json({
-        msg : 'Oops!, couldnt save your data in the DB; Try again...'
-      })
-    } else {
-      console.log(insertResult.rows)
-      // Upon storing, send a Verification link
-      sendVerificationLink(data.email, data.token, res)
-    }
-  })
+async function storeUserDataInDB(data, res) {
+  let insertResult = await postgresql.insert(data)
+  if(insertResult === undefined) {
+    res.status(502).json({
+      msg : 'Oops!, couldnt save your data in the DB; Try again...'
+    })
+  } else {
+    console.log(insertResult.rows)
+    // Upon storing, send a Verification link
+    sendVerificationLink(data.email, data.token, res)
+  }
 }
 
-function checkIfUserRegistered(req, res, next) {
-  const fetchQuery = queries.fetchUserData('email', req.body.email)
-  console.log(fetchQuery)
-  client.query(fetchQuery, (fetchErr, fetchResult) => {
-    if(fetchErr) {
-      console.log(fetchErr)
-      res.status(502).json({
-      msg : 'Oops!, something went wrong; Try again...'
-      })
-    }
-    else {
+async function checkIfUserRegistered(req, res, next) {
+  let fetchResult = await postgresql.fetch('email', req.body.email)
+  if(fetchResult === undefined) {
+    res.status(502).json({
+    msg : 'Oops!, something went wrong; Try again...'
+    })
+  } else {
+    console.log(fetchResult.rows)
       if(fetchResult.rows.length === 0) next()
-      else if(!fetchResult.rows[0].verified) {
+      else if(!fetchResult.rows[0].verified)
         //Hasnt verified yet *eg:data stored on db but email sending fails
-        sendVerificationLink(fetchResult.rows[0].email[0], fetchResult.rows[0].token[0], res)
-      } else {
+      sendVerificationLink(fetchResult.rows[0].email[0], fetchResult.rows[0].token[0], res)
+      else
         res.status(201).json({
           msg : 'You have already been verified - Just Sign Up with the credentials'
         })
-      }
     }
-  })
 }
 
-function updateUsersVerifiedStatus(req, res, next) {
-  const fetchQuery = queries.fetchUserData('token', req.params.token)
-  console.log(req.params.token, 'in fetch')
-
-  client.query(fetchQuery, (fetchErr, fetchResult) => {
-    if(fetchErr) {
-      console.log(fetchErr)
-      res.status(502).json({
-        msg : 'Couldnt retrive data; Try clicking the link again after some time...'
-      })
-    } else {
-      console.log(fetchResult.rows[0].expires)
-      const updateQuery = queries.updateVerifiedStatus(fetchResult.rows[0].id)
-
-      //Checking whether the verification link is still valid or not to update verified status
-      let isLinkValid = !isExpired(fetchResult.rows[0].expires)
-      if(isLinkValid) {
-        client.query(updateQuery, (updateErr, updateResult) => {
-          if(updateErr) {
-            console.log(updateErr)
-            res.status(502).json({
-              msg : 'Couldnt set data; Try clicking the link again after some time...'
-            })
-          } else {
-            console.log(updateResult)
-            res.status(201).json({
-              msg : 'Congratulations, You are verified...'
-            })
-          }
+async function updateUsersVerifiedStatus(req, res, next) {
+  console.log(req.params.token)
+  let fetchResult =
+  await postgresql.fetch('token', req.params.token)
+  if(fetchResult === undefined) {
+    res.status(502).json({
+      msg : 'Couldnt retrive data; Try clicking the link again after some time...'
+    })
+  } else {
+    console.log(fetchResult.rows, 'Verification')
+    let isLinkValid =
+    await isExpired(fetchResult.rows[0].expires, fetchResult.rows[0].id, res)
+    if(!isLinkValid) {
+      let updateResult = await postgresql.update(fetchResult.rows[0].id)
+      if(updateResult === undefined)
+        res.status(502).json({
+          msg : 'Couldnt set data; Try clicking the link again after some time...'
+        })
+      else {
+        console.log(updateResult)
+        res.status(201).json({
+          msg : 'Congratulations, You are verified...'
         })
       }
     }
-  })
+  }
 }
 
-function isExpired(time) {
+//Delete the users data if the verification link has expired
+async function isExpired(time, id, res) {
   let currentTime = Date.now()
   if(currentTime > time) {
-    const deleteQuery = queries.deleteUserData(fetchResult.rows[0].id)
-
-    //Delete the users data if the verification link has expired
-    client.query(deleteQuery, (deleteErr, deleteResult) => {
-      if(deleteErr) {
-        console.log(deleteErr)
-        res.status(502).json({
+    let deleteResult = await postgresql.deleteData(id)
+    if(deleteResult === undefined)
+      res.status(502).json({
         msg : 'Oops!, something went wrong; Try again...'
-        })
-      } else {
-        console.log(deleteResult)
-        res.status(201).json({
-        msg : 'The verification link has expired; Kindle re-register at the registration page'
-        })
-      }
-    })
-  } else return false
+      })
+    else {
+      console.log(deleteResult)
+      res.status(201).json({
+      msg : 'The verification link has expired; Kindle re-register at the registration page'
+      })
+    }
+  }
+  else return false
 }
 
-function getHashIfAvailable(email, compare, res) {
-  const fetchQuery = queries.fetchUserData('email', email)
-  console.log(fetchQuery)
-
-  client.query(fetchQuery, (fetchErr, fetchResult) => {
-    if(fetchErr) {
-      console.log(fetchErr)
-      res.status(502).json({
-      msg : 'Couldnt retrive data; Try again after some time...'
+async function getHashIfAvailable(email, res) {
+  let fetchResult = await postgresql.fetch('email', email)
+  if(fetchResult === undefined)
+    res.status(502).json({
+    msg : 'Couldnt retrive data; Try again after some time...'
+    })
+  else {
+    console.log(fetchResult.rows)
+    if(fetchResult.rows.length === 0)
+      res.status(201).json({
+        msg : "Looks like you havent created an account yet; Create one soon"
       })
-    } else {
-      console.log(fetchResult.rows[0])
-      if(fetchResult.rows.length === 0)
-        res.status(201).json({
-          msg : "Looks like you havent created an account yet; Create one soon"
-        })
-      else compare(fetchResult.rows[0].password[0])
-    }
-  })
+    else if(!fetchResult.rows[0].verified)
+      res.status(201).json({
+        msg : "Your account has not been verified yet!; Check your inbox or spam folder..."
+      })
+    else return fetchResult.rows[0].password[0]
+  }
 }
 
 const handleDB = {
