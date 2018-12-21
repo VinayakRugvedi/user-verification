a638771f2fc65a79e6290944ae9db76d369fdf55
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-21 09:03:57 +0530

    Add async functions to perform querying

--- model/mongodb.js
+++ model/mongodb.js
@@ -1,24 +1,84 @@
 const mongoose = require('mongoose')
 const connectionString = 'mongodb+srv://vinayak:1.618033@cluster0-synfk.mongodb.net/users?retryWrites=true'
 
-mongoose.connect(connectionString, { useNewUrlParser: true })
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useFindAndModify: false //For a deprecated warning
+})
 
 const db = mongoose.connection
+
+const userDataSchema = new mongoose.Schema({
+  token : String,
+  expires : Number,
+  email : String,
+  password : String,
+  verified : Boolean
+})
+
+//Compiling schema into model
+const userData = mongoose.model('data', userDataSchema)
 
 db.on('error', (error) => {
   console.log(error)
   console.log('Error connectiong to the database!')
 })
+
 db.once('open', () => {
   console.log('Successfully connected to the database...')
 })
 
-const userDataSchema = new mongoose.Schema({
-  id : Schema.Types.ObjectId,
-  token : String,
-  expires : Number,
-  email : String,
-  password : String
-})
+async function insert(data) {
+  let info = new userData({
+    token : data.token,
+    expires : data.expires,
+    email : data.email,
+    password : data.password,
+    verified : data.verified
+  })
 
-const userData = mongoose.model('userData', userDataSchema)
+  let insertResult =
+  await info.save()
+  .catch((insertError) => console.log(insertError))
+  console.log(insertResult)
+  //insertResult is a document object
+  return insertResult
+}
+
+async function fetch(key, value) {
+  let fetchResult =
+  await userData.find({ [key] : value }) //Computed Property
+  .catch((fetchError => console.log(fetchError)))
+    console.log(fetchResult)
+  //fetchResult is an array of objects
+  return fetchResult.length
+         ? fetchResult[0]
+         : {}
+}
+
+async function update(id) {
+  let updateResult =
+  await userData.findOneAndUpdate({ _id : id }, { $set : { verified : true }})
+  .catch((updateError) => console.log(updateError))
+  console.log(updateResult)
+  //updateResult is a document object
+  return updateResult
+}
+
+async function deleteData(id) {
+  let deleteResult =
+  await userData.findOneAndDelete({ _id : id })
+  .catch((deleteError) => console.log(deleteError))
+  console.log(deleteResult)
+  //deleteResult is a document object
+  return deleteResult
+}
+
+const mongodb = {
+  insert,
+  fetch,
+  update,
+  deleteData
+}
+
+module.exports = mongodb
