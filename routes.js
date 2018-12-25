cc0355efea0e807a43b85ada21cdfdd373597aad
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-26 00:00:18 +0530

    Update naming convention

--- routes.js
+++ routes.js
@@ -3,20 +3,20 @@ const router = express.Router()
 
 const performValidations = require('./controllers/validateEmailAndPassword')
 const hashPasswordAndStoreInDB = require('./controllers/hashPasswordAndStoreInDB')
-const authenticateTheUser = require('./controllers/authenticateTheUser')
+const authenticateUser = require('./controllers/authenticateUser')
 const handleDB = require('./controllers/handleDB')
 
-router.use('/verifyUser', performValidations)
+router.use('/signUp', performValidations)
 
 //Cross check if user has already registered
-router.use('/verifyUser', handleDB.checkIfUserRegistered)
+router.use('/signUp', handleDB.checkIfUserRegistered)
 
-router.post('/verifyUser', hashPasswordAndStoreInDB)
+router.post('/signUp', hashPasswordAndStoreInDB)
 
 //When the user clicks the verification link, update his/her status in DB
-router.get('/verifyMe/:token', handleDB.updateUsersVerifiedStatus)
+router.get('/verify/:token', handleDB.updateUsersVerifiedStatus)//me
 
 router.use('/authenticateUser', performValidations)
-router.post('/authenticateUser', authenticateTheUser)
+router.post('/authenticateUser', authenticateUser)//the
 
 module.exports = router
